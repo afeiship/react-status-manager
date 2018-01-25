ec5887d6f7c2ac49fbae6481bd735bf93a410252
afeiship <1290657123@qq.com>
2018-01-25 21:28:42 +0800

    issue: format

--- src/components/react-status-manager.js
+++ src/components/react-status-manager.js
@@ -1,4 +1,4 @@
-import { Component, createElement } from 'react';
+import { Component } from 'react';
 
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -19,7 +19,7 @@ export default class extends Component{
   render(){
     const { className, status, statusList, children, ...props } = this.props;
     return (
-      <div {...props} className={classNames('react-status-manager',className)} data-status={status}>
+      <div {...props} className={ classNames('react-status-manager',className) } data-status={status}>
         {
           children.map((child,index)=>{
             return status === statusList[index] && child;
