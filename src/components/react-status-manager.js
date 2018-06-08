95c94c4daef603dc5e667825a87d7e208c911eb8
afeiship <1290657123@qq.com>
2018-06-08 16:12:04 +0800

    issue: refactor

--- src/components/react-status-manager.js
+++ src/components/react-status-manager.js
@@ -1,31 +1,37 @@
-import { Component } from 'react';
+import { Component, createElement } from 'react';
 
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export default class extends Component{
+export default class extends Component {
   /*===properties start===*/
   static propTypes = {
     className: PropTypes.string,
     statusList: PropTypes.array,
-    status: PropTypes.any
+    status: PropTypes.any,
+    nodeName: PropTypes.any
   };
 
   static defaultProps = {
-    statusList: []
+    statusList: [],
+    nodeName: 'div'
   };
   /*===properties end===*/
 
-  render(){
-    const { className, status, statusList, children, ...props } = this.props;
-    return (
-      <div {...props} className={ classNames('react-status-manager',className) } data-status={status}>
-        {
-          children.map((child,index)=>{
-            return status === statusList[index] && child;
-          })
-        }
-      </div>
-    );
+  get children(){
+    const { status, children, statusList } = this.props;
+    return children.map((child, index) => {
+      return status === statusList[index] ? child : null;
+    });
+  }
+
+  render() {
+    const { className, status, statusList, children, nodeName, ...props } = this.props;
+    return createElement(nodeName,{
+      className: classNames('react-status-manager', className),
+      'data-status': status,
+      children: this.children,
+      ...props
+    });
   }
 }
