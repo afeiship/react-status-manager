d55d9ed4d238bc90d6378cba207b3f28da8738d6
afeiship <1290657123@qq.com>
2025-01-06 16:31:29 +0800

    feat: use fc compoent

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -1,6 +1,6 @@
-import React, { Fragment, Component, ReactNode } from 'react';
+import React, { Fragment, ReactNode, FC, PropsWithChildren } from 'react';
 
-export interface ReactStatusManagerProps extends React.PropsWithChildren<any> {
+export interface ReactStatusManagerProps extends PropsWithChildren<any> {
   className?: string;
   items: any[];
   value?: any;
@@ -8,27 +8,26 @@ export interface ReactStatusManagerProps extends React.PropsWithChildren<any> {
   asProps?: any;
 }
 
-export default class ReactStatusManager extends Component<ReactStatusManagerProps> {
-  static defaultProps = {
-    items: [],
-    asProps: {},
-  };
+const defaults: ReactStatusManagerProps = {
+  items: [],
+  asProps: {},
+};
 
-  get children() {
-    const { value, children, items } = this.props;
-    return children.map((child: ReactNode, index: number) => {
+const ReactStatusManager: FC<ReactStatusManagerProps> = (props) => {
+  const { value, children, items, as: AsComponent = Fragment, asProps = {}, ...restProps } = { ...defaults, ...props };
+
+  const renderChildren = () => {
+    return React.Children.map(children, (child: ReactNode, index: number) => {
       const status = items[index];
       const isNestedArray = Array.isArray(status);
-      if (typeof status !== value && isNestedArray) {
+      if (typeof status !== typeof value && isNestedArray) {
         return status.includes(value) ? child : null;
       }
       return value === items[index] ? child : null;
     });
-  }
+  };
 
-  render() {
-    const { value, items, children, as, asProps, ...props } = this.props;
-    const AsComponent = as || Fragment;
-    return React.createElement(AsComponent, { children: this.children, ...asProps, ...props });
-  }
-}
+  return React.createElement(AsComponent, { ...asProps, ...restProps, children: renderChildren() });
+};
+
+export default ReactStatusManager;
